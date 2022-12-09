0eb98beeaa8a2884fa5ec44b2ad3d88bdc322a1b
RyanNdegwa <ryanndegwa3560@gmail.com>
2022-12-09 08:21:25 -0800

    Uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - alphabets
+ *
+ * Return: 0
+*/
+int main(void)
+{
+char ch = 'A';
+while (ch <= 'Z')
+{
+printf("%c", ch);
+ch++;
+}
+printf("\n");
+return (0);
+}
+
