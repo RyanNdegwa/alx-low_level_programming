d6c584d4beb4cfd1f0aca65732a9e9d55aac3ac7
RyanNdegwa <ryanndegwa3560@gmail.com>
2022-12-09 12:09:58 -0800

    Base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/**
+ * main - print a random positive or negative num
+ * Return: 0
+*/
+int main(void)
+{
+int num;
+char letter;
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+for (letter = 'a'; letter <= 'f'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
+}
+
