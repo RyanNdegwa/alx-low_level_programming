82af7850473ec76e9b4bb113ffcbebc8e75f06df
RyanNdegwa <ryanndegwa3560@gmail.com>
2022-12-09 10:04:14 -0800

    m

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,19 +7,21 @@
 */
 int main(void)
 {
-char cha = 'a';
+char cha;
+cha = 'a'
 while (cha <= 'z')
 {
 putchar(cha);
-cha ++;
+cha++;
 }
-char ch = 'A';
+char ch;
+ch = 'A'
 while (ch <= 'Z')
 {
 putchar(ch);
 ch++;
 }
-
+putchar('\n');
 return (0);
 }
 
