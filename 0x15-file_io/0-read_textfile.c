d84b145583edba90635e2231f02a0d300ceaa998
RyanNdegwa <ryanndegwa3560@gmail.com>
2023-02-08 12:03:23 -0800

    function that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,38 +5,44 @@
 * @letters: Number of letter it should read and print.
 * Return: Actual number of letter it could read and print.
 */
-ssize_t read_textfile(const char *filename, size_t letters) {
-    if (!filename) {
-        return 0;
-    }
-
-    int file = open(filename, O_RDONLY);
-    if (file == -1) {
-        return 0;
-    }
-
-    char *buf = malloc(letters);
-    if (!buf) {
-        close(file);
-        return 0;
-    }
-
-    ssize_t read_result = read(file, buf, letters);
-    if (read_result == -1) {
-        free(buf);
-        close(file);
-        return 0;
-    }
-
-    ssize_t write_result = write(STDOUT_FILENO, buf, read_result);
-    free(buf);
-    close(file);
-
-    if (write_result == -1 || write_result != read_result) {
-        return 0;
-    }
-
-    return read_result;
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+if (!filename)
+{
+return (0);
 }
+
+file = open(filename, O_RDONLY);
+if (file == -1)
+{
+return (0);
+}
+
+buf = malloc(letters);
+if (!buf)
+{
+close(file);
+return (0);
+}
+
+read_result = read(file, buf, letters);
+if (read_result == -1)
+{
+free(buf);
+close(file);
+return (0);
+}
+write_result = write(STDOUT_FILENO, buf, read_result);
+free(buf);
+close(file);
+
+if (write_result == -1 || write_result != read_result)
+{
+return (0);
+}
+
+return (read_result);
+}
+
 
 
